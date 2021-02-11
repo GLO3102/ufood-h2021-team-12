cf43c94ffb451e65a2b67d557858b63c1728e5e9
David Malenfant <david.malenfant.2@ulaval.ca>
2021-02-11 13:30:12 -0500

    Basic ratings

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,8 +1,43 @@
 <template>
   <div>
-    <h1>Restaurant</h1>
+    <div class="restaurantBannerCore">
+      <h1 class="restaurantBannerName">Restaurant Normandin</h1>
+      <div class="restaurantBannerAlias">(Restaurant Normandin Duberger)</div>
+      <div class="restaurantBannerRatings">
+        <div class="ratingBlock">
+          <div class="restaurantRating colorSchemeGlobal">
+            <div class="ratingHeader">
+              <h3>Restaurant rating</h3> <a class="whatDoesThisMean" href="">what does this mean?</a>
+            </div>
+            <div class="ratingElementFormat">★ ★ ★</div>
+          </div>
+          <div class="restaurantRating colorSchemeFriend">
+            <div class="ratingHeader">
+              <h3>Restaurant rating</h3> <a class="whatDoesThisMean" href="">what does this mean?</a>
+            </div>
+            <div class="ratingElementFormat">★ ★ ★ ★</div>
+          </div>
+        </div>
+        <div class="ratingBlock">
+          <div class="restaurantRating colorSchemeGlobal">
+            <div class="ratingHeader">
+              <h3>Cost rating</h3> <a class="whatDoesThisMean" href="">what does this mean?</a>
+            </div>
+            <div class="ratingElementFormat">$$</div>
+          </div>
+          <div class="restaurantRating colorSchemeFriend">
+            <div class="ratingHeader">
+              <h3>Cost rating</h3> <a class="whatDoesThisMean" href="">what does this mean?</a>
+            </div>
+            <div class="ratingElementFormat">$</div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div>GLO-3102 Restaurant page</div>
   </div>
 </template>
 
-<style></style>
+<style>
+@import "restaurant.css";
+</style>
