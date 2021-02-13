81733fb6caf0d92546b2639d64eb4c1131dfd4ec
David Malenfant <david.malenfant.2@ulaval.ca>
2021-02-13 01:35:58 -0500

    A bit more of a shell

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <div class="restaurantBannerCore">
-      <h1 class="restaurantBannerName">Restaurant Normandin</h1>
-      <div class="restaurantBannerAlias">(Restaurant Normandin Duberger)</div>
+      <h1 class="restaurantBannerName">Normandin Duberger</h1>
+      <a class="restaurantBannerFoodChain" href="https://restaurantnormandin.com/en/">Normandin</a>
+      <div class="restaurantBannerAddressInfo">
+        <div><a href="https://www.google.com/maps/place/Restaurant+Normandin/@46.8114671,-71.2968759,15z/data=!4m5!3m4!1s0x0:0x472d926e469834b8!8m2!3d46.8114671!4d-71.2968759">2185 Boulevard Père-Lelièvre, Québec, QC G1P 2X2</a></div>
+        <div><a href="callto:4186834967">call (418) 683-4967</a></div>
+        <div><a class="restaurantDescriptorTag" href="">Dine-in</a> | <a class="restaurantDescriptorTag" href="">Curbside pickup</a> | <a class="restaurantDescriptorTag" href="">No-contact delivery</a></div>
+      </div>
       <div class="restaurantBannerRatings">
         <div class="ratingBlock">
           <div class="restaurantRating colorSchemeGlobal">
@@ -34,7 +39,32 @@
         </div>
       </div>
     </div>
-    <div>GLO-3102 Restaurant page</div>
+    <div class="restaurantInfoCore">
+      <div class="restaurantTypeInfo">
+        <a class="restaurantDescriptorTag" href="">Pizza</a>, <a class="restaurantDescriptorTag" href="">Family Restaurant</a>, <a class="restaurantDescriptorTag" href="">Sports Bar</a>
+      </div>
+      <div class="restaurantInfoDualPanel">
+        <div class="restaurantPhotoGalleryCore">
+          Oh no it's grid time
+        </div>
+        <div class="restaurantHoursCore">
+          All hours stuff
+        </div>
+      </div>
+      <div class="restaurantMapCore">
+        I'd put a map here... if I had one!
+      </div>
+    </div>
+    <div class="restaurantSocialCore">
+      <div class="restaurantReviewCore">
+        <div class="restaurantReviewBlock colorSchemeFriend">
+          All my friend reviews show up in yellow
+        </div>
+        <div class="restaurantReviewBlock colorSchemeGlobal">
+          People who are not my friend show up in blue
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
